2bc57d9288e9c364c1a5bcc6dc0bda478fb2cd21
Richa Singh <richasingh30469@gmail.com>
2022-09-07 22:29:18 +0530

    1st Spring commit

---
+++ src/main/java/com/example/demospringboot7pm/DemoSpringBoot7pmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot7pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot7pmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot7pmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demospringboot7pm/controller/DemoController.java
@@ -0,0 +1,57 @@
+package com.example.demospringboot7pm.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //to create method :  access_spec  return_type method_name() {CODE}
+
+    @RequestMapping("/hello")     //  this annotation is for calling endpoints
+    public void hello()
+    {
+        System.out.println(" Welcome to spring boot .......");
+    }
+
+    @RequestMapping("/Welcome")
+    public void welcome()                 //  without return for console o/p
+    {
+        System.out.println("Welcome to Spring Boot .........");
+    }
+
+
+
+    @RequestMapping("/age")          //return_type for browser o/p
+    public int age()
+    {
+        int varsha=27;
+        return varsha;
+    }
+    @RequestMapping("/name")
+    public String name(String var)
+    {
+        var="String var as a parameter inside method name ......";   // experiment
+        System.out.println("var : "+var);
+        String name="Varsha Thakur";
+       // System.out.println("Richa Rajawat...."+name);      it is Varsha trial testing
+
+        return name;
+    }
+    @RequestMapping("/salary")
+    public double salary()
+    {
+        double salary=50000.00;
+        return salary;
+    }
+
+    @RequestMapping("/char")
+    public char startingLetter()
+    {
+        char sl='V';
+        System.out.println(" My starting letter is : "+sl);
+        return sl;
+
+    }
+}

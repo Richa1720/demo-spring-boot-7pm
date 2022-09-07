7a50077a21bb0744fbe3775d45c4a892ec83a44b
Richa Singh <richasingh30469@gmail.com>
2022-09-07 23:28:47 +0530

    CalculatorController

--- src/main/java/com/example/demospringboot7pm/controller/CalculatorController.java
+++ src/main/java/com/example/demospringboot7pm/controller/CalculatorController.java
@@ -1,4 +1,46 @@
 package com.example.demospringboot7pm.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+
+    @RequestMapping("/addition")
+    public int addition()
+    {
+        int a=100;
+        int b=50;
+        int add=a+b;
+
+        return add;
+    }
+    @RequestMapping("/subtraction")
+    public int subtraction()
+    {
+        int a=66;
+        int b=33;
+        int sub=a-b;
+
+        return sub;
+    }
+    @RequestMapping("/multiplication")
+    public int multiplication()
+    {
+        int a=5;
+        int b=9;
+        int mul=a*b;
+
+        return mul;
+    }
+
+    @RequestMapping("/division")
+    public int divide()
+    {
+        int a=63;
+        int b=7;
+        int div=a/b;
+
+        return div;
+    }
 }

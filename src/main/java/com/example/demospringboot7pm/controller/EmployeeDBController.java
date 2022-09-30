efbc252ccdc559d6d50b607f7f7b0396a7561339
Richa Singh <richasingh30469@gmail.com>
2022-09-30 23:36:19 +0530

    Autowired DB CRUD controller(save,findAll,findById,DeleteById)

---
+++ src/main/java/com/example/demospringboot7pm/controller/EmployeeDBController.java
@@ -0,0 +1,47 @@
+package com.example.demospringboot7pm.controller;
+
+import com.example.demospringboot7pm.model.Employee;
+import com.example.demospringboot7pm.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EmployeeDBController {
+
+    //C FOR CREATE/ADD
+    @Autowired
+    EmployeeService service;
+    @PostMapping("/add_emp")
+    public String addEmp(@RequestBody Employee employee)
+    {
+       // return "Successfully added student to EmployeeDBController using requestBody ";
+        return service.add(employee);
+    }
+
+    //R FOR READ / GET
+    @GetMapping("/get_employee_db")
+    public List<Employee> getEmployeesArrayList()
+    {
+        return service.getAllEmployee();
+    }
+
+    //U UPDATE /PUT
+
+    @PutMapping("/update_employee_db")
+    public String updateEmployeedb(@RequestParam long id,@RequestParam String name,@RequestParam String dept)
+    {
+       return service.updateEmployeeDB(id,name,dept);
+    }
+
+    //DELETE
+    @DeleteMapping("/delete_employee_db")
+    public String deleteEmployeeDB(@RequestParam long id)
+    {
+        return service.deleteEmp(id);
+    }
+
+
+}
---
+++ src/main/java/com/example/demospringboot7pm/service/EmployeeService.java
@@ -0,0 +1,51 @@
+package com.example.demospringboot7pm.service;
+
+import com.example.demospringboot7pm.model.Employee;
+import com.example.demospringboot7pm.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository repository;
+    public String add(Employee employee)
+    {
+       // return "success";
+        repository.save(employee);            //insert student data in database(Employee)
+        return "Successfully added employee to DB ";
+
+    }
+
+    public List<Employee> getAllEmployee()
+    {
+       return repository.findAll();         //  get all the data from table
+    }
+
+    //UPDATE
+
+    public String updateEmployeeDB(long id,String name,String dept)
+    {
+        //1 find out record
+        Employee employee=repository.getById(id);
+
+        //2 update the value and save to the database
+        employee.setName(name);
+        employee.setDept(dept);             //updating two values simultaneously in same method
+        repository.save(employee);
+        return "Successfully updated name as "+name;
+    }
+
+    //U UPDATE /PUT
+
+    public String deleteEmp(long id)
+    {
+        repository.deleteById(id);
+
+        return "Successfully deleted employee from the database";
+    }
+}

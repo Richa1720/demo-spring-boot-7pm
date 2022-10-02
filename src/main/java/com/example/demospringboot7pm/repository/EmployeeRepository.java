80b77fe0e9961d63601762aa350d2830cc756272
Richa Singh <richasingh30469@gmail.com>
2022-10-02 21:51:39 +0530

    add dev created method EmployeeDBController

--- src/main/java/com/example/demospringboot7pm/repository/EmployeeRepository.java
+++ src/main/java/com/example/demospringboot7pm/repository/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package com.example.demospringboot7pm.repository;
 
 import com.example.demospringboot7pm.model.Employee;
+import com.example.demospringboot7pm.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+    public Employee findByName(String name);
 
 }
